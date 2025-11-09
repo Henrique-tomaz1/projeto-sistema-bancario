34a7ed9f2414d47b42eeb44d0087046d4d655adb
henrique-tomaz1 <hto_h@hotmail.com>
2025-11-09 18:14:45 -0300

    Atualizei o contagem regressiva

--- Contagem regressiva com função.py
+++ Contagem regressiva com função.py
@@ -6,3 +6,14 @@ def contagem_regressima (numero):
             break
 
 contagem_regressima(5)
+
+def maior_numero (lista_de_numeros):
+    maior_numero = lista_de_numeros[0]
+    for numero in lista_de_numeros:
+        if numero > maior_numero:
+            maior_numero = numero
+    return maior_numero
+
+lista = [5, 10, 25, 22, 99]
+maior_numero_da_lista = maior_numero(lista)
+print(f"O Maior numero da lista é {maior_numero_da_lista}")
