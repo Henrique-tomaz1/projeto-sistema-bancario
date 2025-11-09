66f742c6f6617fd22643751bdf8e27f0db97454f
henrique-tomaz1 <hto_h@hotmail.com>
2025-11-09 13:34:48 -0300

    adicionei exercicios com função

---
+++ Contagem regressiva com função.py
@@ -0,0 +1,8 @@
+def contagem_regressima (numero):
+    while True:
+        print (numero)
+        numero -= 1
+        if numero < 0:
+            break
+
+contagem_regressima(5)
--- Primeiro programa.py
+++ Primeiro programa.py
@@ -1,3 +1,32 @@
-msg = 'olá, mundo'
+# # msg = 'olá, mund
 
-print (msg)
+# def ola_mundo ():
+#     print("Olá Mundo")
+
+# ola_mundo()
+# ola_mundo()
+
+
+
+# def dizer_ola_para(nome):
+#     print("Olá, " + nome + "! Você está bem?")
+
+
+# dizer_ola_para("Henrique")
+# dizer_ola_para("Maju")
+
+def soma_numeros(num1, num2):
+    resultado_da_soma = num1 + num2
+    return resultado_da_soma
+
+minha_soma = soma_numeros(1, 2)
+
+print ("A Soma é ", minha_soma )
+
+def nova_soma(num1, num2):
+    resultado_da_soma = num1 + num2
+    return resultado_da_soma
+
+minha_soma = soma_numeros (1, 3)
+
+print("Reresultado é ", minha_soma)
